f14708069ddc8d16ca48a964637efdac6418c30b
suren <suren>
2023-01-14 17:46:19 +0000

    ws chat app: initial commit with k8s manifests

---
+++ chat/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/olahol/melody"
+)
+
+func main() {
+	r := gin.Default()
+	m := melody.New()
+
+	r.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "index.html")
+	})
+
+	r.GET("/host", func(c *gin.Context) {
+		hostname, err := os.Hostname()
+		if err != nil {
+			hostname = "not available"
+		}
+		c.String(http.StatusOK, hostname)
+	})
+
+	r.GET("/connections", func(c *gin.Context) {
+		activeSessions, _ := m.Sessions()
+		c.String(http.StatusOK, strconv.Itoa(len(activeSessions)))
+	})
+
+	r.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
+
+	r.Run(":5000")
+}
